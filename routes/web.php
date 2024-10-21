88bf6009d368d1c6b51c955c098c68ba7e513573
Nitin-Tiwari-11 <nitintiwari8983@gmail.com>
2024-10-22 02:40:57 +0530

    --

--- routes/web.php
+++ routes/web.php
@@ -36,9 +36,6 @@ Route::get('/', function () {
     return view('auth/login');
 });
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
